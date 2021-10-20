a573c2f01b7d32ffb0532673aa599c7bf7ff5543
Amy <amylynnroberts@gmail.com>
2021-10-19 20:47:45 -0500

    I don't even know...

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -3,7 +3,10 @@ package org.launchcode.spaday.controllers;
 import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("user")
@@ -15,20 +18,30 @@ public class UserController {
     }
 
     @PostMapping
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, Errors errors, String verify) {
+        model.addAttribute(new User());
         model.addAttribute("user", user);
         model.addAttribute("verify", verify);
         model.addAttribute("username", user.getUsername());
         model.addAttribute("email", user.getEmail());
-        if (user.getPassword().equals(verify)) {
+/*        if (user.getPassword().equals(verify)) {
            return "user/index";
         }
         else {
             model.addAttribute("error", "Passwords do not match");
             return "user/add";
+        }*/
+        if (errors.hasErrors()) {
+            model.addAttribute("error", "Bad data!");
+            return "user/add";
+        } else {
+            return "user/index";
         }
-
     }
+    private void checkPassword(User user){
+        if (user.getPassword() == user.getVerifyPassword()&& user.getPassword() != null && user.getVerifyPassword() != null){
 
+        }
+    }
 
 }
--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,14 +1,32 @@
 package org.launchcode.spaday.models;
+import javax.validation.constraints.*;
 
 public class User {
+
+    @NotBlank(message = "Invalid Username. Try again.")
+    @Size(min = 5, max = 15, message = "Name must be between 5 and 15 characters")
     private String username;
+
+    @Email(message = "Invalid email. Try again.")
     private String email;
+
+    @NotBlank(message = "Password is required.")
+    @Min(6)
     private String password;
+
+    private String verifyPassword;
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+    }
 
     public User() {
 
     }
-
     public User(String username, String email, String password) {
         this();
         this.username = username;
